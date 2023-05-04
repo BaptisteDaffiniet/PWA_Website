6beb72a45ec7cbc8db6d8a7d1d78cd2d6cfacb2c
Daffiniet Baptiste <61876912+BaptisteDaffiniet@users.noreply.github.com>
2023-05-04 16:35:33 +0200

    webPush verification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker'
+import { SwPush, SwUpdate } from '@angular/service-worker'
 import { DataService } from './data.service';
 import { Router } from '@angular/router'
 
@@ -10,10 +10,10 @@ import { Router } from '@angular/router'
 })
 export class AppComponent {
   title = 'websitePWA';
-
+  private readonly publicKey = "BOsRIBtmJWMYm8qitNbIWds9vRvOb4F5BCJz1l7PfISZba0uC61jVjv60lwBgl0Ur46jc-nOLhp7WowsUaCSW3E";
   update: boolean = false;
 
-  constructor(updates: SwUpdate, private data: DataService, private router: Router) {
+  constructor(updates: SwUpdate, private data: DataService, private router: Router, private swPush: SwPush) {
 
     updates.available.subscribe(event => {
       //this.update = true;
@@ -21,11 +21,23 @@ export class AppComponent {
     })
   }
 
+  ngOnInit() {
+    this.pushSubscription();
+  }
+
   goToPage(pageName: string): void {
     console.log("Go To Page" + pageName)
     this.router.navigate([pageName]);
   }
 
-
+  pushSubscription() {
+    if (!this.swPush.isEnabled) {
+      console.log("Notification is not enabled");
+      return;
+    }
+    this.swPush.requestSubscription({
+      serverPublicKey: this.publicKey,
+    }).then(sub => console.log(JSON.stringify(sub))).catch(err => console.log(err))
+  }
 
 }
