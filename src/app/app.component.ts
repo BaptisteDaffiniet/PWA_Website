9590a0b67613f8676994d99ae659b0b23284ef88
Daffiniet Baptiste <61876912+BaptisteDaffiniet@users.noreply.github.com>
2023-05-06 11:44:21 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,46 +22,46 @@ export class AppComponent {
     })
   }
 
-  ngOnInit() {
+  /*ngOnInit() {
     this.pushSubscription();
-  }
+  }*/
 
   goToPage(pageName: string): void {
     console.log("Go To Page" + pageName)
     this.router.navigate([pageName]);
   }
 
-  pushSubscription() {
-    if (!this.swPush.isEnabled) {
-      console.log('Notification is not enabled')
-    } else {
-      this.swPush.requestSubscription({
-        serverPublicKey: this.publicKey,
-      }).then(sub => console.log("try 1"))
-        .catch(err => console.log("error"))
-    }
-  }
-  subscribeToPushNotifications() {
-    this.swPush.requestSubscription({
-      serverPublicKey: this.publicKey
-    }).then(subscription => {
-      console.log('Push notification subscription successful', subscription);
-      // Save the subscription to your server database
-      this.isSubscribed = true;
-    }).catch(error => {
-      console.error('Error subscribing to push notifications', error);
-    });
-  }
-
-  unsubscribeFromPushNotifications() {
-    this.swPush.unsubscribe().then(() => {
-      console.log('Push notification unsubscription successful');
-      // Remove the subscription from your server database
-      this.isSubscribed = false;
-    }).catch(error => {
-      console.error('Error unsubscribing from push notifications', error);
-    });
-  }
+  /* pushSubscription() {
+     if (!this.swPush.isEnabled) {
+       console.log('Notification is not enabled')
+     } else {
+       this.swPush.requestSubscription({
+         serverPublicKey: this.publicKey,
+       }).then(sub => console.log("try 1"))
+         .catch(err => console.log("error"))
+     }
+   }
+   subscribeToPushNotifications() {
+     this.swPush.requestSubscription({
+       serverPublicKey: this.publicKey
+     }).then(subscription => {
+       console.log('Push notification subscription successful', subscription);
+       // Save the subscription to your server database
+       this.isSubscribed = true;
+     }).catch(error => {
+       console.error('Error subscribing to push notifications', error);
+     });
+   }
+ 
+   unsubscribeFromPushNotifications() {
+     this.swPush.unsubscribe().then(() => {
+       console.log('Push notification unsubscription successful');
+       // Remove the subscription from your server database
+       this.isSubscribed = false;
+     }).catch(error => {
+       console.error('Error unsubscribing from push notifications', error);
+     });
+   }*/
 }
 
 
